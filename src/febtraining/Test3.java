d9d0852e78aecf2e5b935f891202315b875bee20
vaisinghequinix <vaisingh@equinix.com>
2023-03-04 10:38:01 +0530

    data type

--- src/febtraining/Test3.java
+++ src/febtraining/Test3.java
@@ -1,7 +1,71 @@
 package febtraining;
 
 public class Test3 {
-public static void main(String[] args) {
-	System.out.println("for issue in github");
-}
+	public static void main(String[] args) {
+		// Data Types 
+		
+		// numeric type    10+20 = 30 
+		// floating data - precision
+		// char
+		// true / false - boolean
+		
+		
+		// Decimal Number System 
+		// 0,1,2,3,4,5,6,7,8,9
+		
+		// Binary 
+		// 0,1
+		
+		// Octal 
+		// 0,1,2,3,4,5,6,7
+		
+		// Hexadecimal 
+		// 0,1,2,3,4,5,6,7,8,9,A(10),B(11),C(12),D(13),E(14),F(15)
+		
+		// Byte and bit
+		// 1 Byte = 8 bit
+		// 1 bit = 0,1
+		
+		
+		// Numeric DataType 
+		// byte - 1 byte = 8 bit , (2 power 8 -1) // 255 positive,
+		
+		// 256 - 128 = negative, 127 = positive 
+		// byte range of byte  =  -128 to +127
+		// byte = 8 bit ,  (-2power7) to (+2power7-1) 
+		
+		
+		// short - 2 byte = 16 bit , (2 power 16 -1), 65K
+		//  (-2power15) to (+2power15-1) 
+		
+		
+		// int - 4 byte = 32 bit (2 power 32 -1) // 
+		// long - 8 byte  = 64 bit (2 power 64 -1) // 
+		// (-2 power 63) to ((2 power 63)-1)
+		
+		// Casting concept // 
+		byte b1 = (byte)300;// 300 - 256 = 44
+		System.out.println(b1);
+		
+		int k = 015; //  octal 
+		System.out.println(k);
+
+		int m = 0XA1; // Hexadecimal
+		System.out.println(m);
+		
+		
+		
+		
+		
+//		short s1 = 128;
+//		int i1 = 128;
+//		long l = 128;
+//		
+		
+		
+		
+		
+		
+		
+	}
 }
